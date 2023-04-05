3bdc32be6b936099955b2dcd142ea9419f3ffee2
AndOliver46 <anderson_souza75@live.com>
2023-04-05 11:01:25 -0300

    Cleaning files to start the final project

--- src/main/java/com/andoliver46/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/andoliver46/erp/controller/GestaoEmpresasBean.java
@@ -5,32 +5,10 @@ import java.io.Serializable;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
-import com.andoliver46.erp.model.Empresa;
-import com.andoliver46.erp.model.TipoEmpresa;
-
 @Named
 @ApplicationScoped
 public class GestaoEmpresasBean implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
-	private Empresa empresa = new Empresa();
-	
-	public Empresa getEmpresa() {
-		return empresa;
-	}
-	
-	public void salvar() {
-		System.out.println("Empresa: " + empresa.getRazaoSocial() + 
-				empresa.getNomeFantasia() + " " + empresa.getTipo());
-	}
-	
-	public TipoEmpresa[] getTiposEmpresa() {
-		return TipoEmpresa.values();
-	}
-	
-	public String ajuda() {
-		return "AjudaGestaoEmpresas?faces-redirect=true";
-	}
 	
 	
 }
